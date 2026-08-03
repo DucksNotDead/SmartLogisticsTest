e60643d68426bb2b0aee8f615b5672cf661d205c
aleksandrholuenko <zerogormy@mail.ru>
2026-08-03 11:46:13 +0800

    fix: love

--- src/widgets/app-shell/ui/Footer.tsx
+++ src/widgets/app-shell/ui/Footer.tsx
@@ -3,7 +3,7 @@ export function Footer() {
     <footer className="shrink-0 border-t border-border bg-background">
       <div className="mx-auto flex w-full max-w-6xl flex-col gap-3 px-2 py-3 text-sm text-muted-foreground md:flex-row md:items-center md:justify-between md:gap-6 md:px-6">
         <p className="text-xs text-center w-full">
-          Разработано с глубоким уважением для "ООО Умная логистика"
+          Разработано с любовью для "ООО Умная логистика"
         </p>
       </div>
     </footer>
